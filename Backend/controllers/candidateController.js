03c6120faaed90bac82f51b6d38b2adba9a6d01a
arnav-1702 <kulkarniarnav61@gmail.com>
2025-09-30 15:33:21 +0530

    nodemailer

--- Backend/controllers/candidateController.js
+++ Backend/controllers/candidateController.js
@@ -18,8 +18,13 @@ const generateOTP = () =>
 
 // Email transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for 587
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // ================= AUTH ================= //
